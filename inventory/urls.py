aaeced282406342751b4794f5b0dc9eaf222ed35
timkenar <chepkenertimothy@gmail.com>
2025-02-19 16:44:45 +0300

    Untracked Changes

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from .views import item_list, add_item
 
+from . import views
 
 app_name = 'inventory'
 
 urlpatterns = [
     # path('items/', item_list, name='item-list'),
     # path('items/add/', add_item, name='add-item'),
-    path('items/<int:tenant_id>/', item_list, name='item-list'),
-    path('items/<int:tenant_id>/add/', add_item, name='add-item'),
+    # path('items/<int:tenant_id>/', item_list, name='item-list'),
+    # path('items/<int:tenant_id>/add/', add_item, name='add-item'),
+    path('tenant/<int:tenant_id>/items/', item_list, name='item-list'),  # For viewing items
+    path('tenant/<int:tenant_id>/items/add/', add_item, name='add-item'),  # For adding new item
+
+    # path('purchases/', add_item, name='add-item')
 ]
--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +9,7 @@ urlpatterns = [
     path('inventory/', include('inventory.urls')),
     path('core/', include('core.urls')),
     # path('sales/',include('sales.urls'))
-]
+
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
